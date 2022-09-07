31877132f9e868385a4af030aa2af47a6f58ec81
Cengiz Dönmez <c.donmezzz@gmail.com>
2022-09-07 11:29:30 +0300

    fetch using useEffect & random photo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import './App.css';
 
+import { useEffect, useState } from "react";
 function App() {
+
+  const [data, setData] = useState(0);
+  const [image, setImage] = useState(0);
+  
+  useEffect(() => {
+      fetch('https://dog.ceo/api/breeds/image/random')
+    .then(response => {
+      if(response.ok) {
+        return response.json();
+      }
+      console.log(response);
+    })
+    .then(data => {
+      setData(data);
+    })  
+    .catch(error => console.log(error))
+  }, [image]);
+
   return (
     <div className="App">
-      Initial Commit
+      <div className="image-content">
+        <img className="img" src={data.message} alt="" />
+        <button onClick={setImage}>Random</button>
+      </div>
     </div>
   );
 }
